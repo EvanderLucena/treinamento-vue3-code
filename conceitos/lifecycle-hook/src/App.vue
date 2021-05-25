e1b880fd17f70f37412d8377f79f629371cc42be
evander <evander@c4sistemas.com.br>
2021-05-25 17:30:37 -0300

    lifecycle-hook

---
+++ conceitos/lifecycle-hook/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Lifecycle Hooks</h1>
+</template>
+
+<script>
+export default {
+  setup(){
+    onMounted(() => {
+      console.log('Foi montado')
+    }
+
+    )
+  }
+}
+export default {
+  mounted(){
+    console.log("Foi montado option API")
+  }
+}
+</script>
+
+<style>
+
+</style>
