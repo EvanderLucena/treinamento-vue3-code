1794695c7417f94edb9e92f7ac1bdfc5b28fbda3
evander <evander@c4sistemas.com.br>
2021-05-25 10:53:49 -0300

    data binding

--- conceitos/data-binding/App.vue
+++ conceitos/data-binding/App.vue
@@ -1,10 +1,19 @@
 <template>
-  
+<div>
+  <h1 :style="{textDecoration: decoration}">Vue Js: {{name}}</h1>
+  <input type="text" v-model="name">
+  <br>
+  <a :href="link">github</a>
+</div>
 </template>
 
 <script>
 export default {
-
+    data: () => ({
+        name: 'DataBinding',
+        link: 'https://github.com/EvanderLucena',
+        decoration:'underline'
+    })
 }
 </script>
 
